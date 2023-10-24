7a401534e12fa1492681e80fdcafc7fe3df74ab0
dominik.vins <dominik.vins@portabo.cz>
2023-10-24 15:28:34 +0200

    Initial Upload

---
+++ Assets/Scripts/Prog_1/DamageBuff.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageBuff : Buff
+{
+    //TODO odkaz na hr·Ëe
+    float dmg = 5; // Z hr·Ëe
+    protected override void ApplyBuff()
+    {
+        dmg *= buffPower;
+        Timer();
+    }
+    protected override void Timer()
+    {
+        base.Timer();
+        dmg /= buffPower;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        ApplyBuff();
+        Destroy(this);
+    }
+
+}
